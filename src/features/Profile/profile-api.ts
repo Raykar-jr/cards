b773d6569f58c4c6f08084a79dc5d8426e0b12c9
yanakorobova <yanaheaven@yandex.ru>
2023-03-07 18:48:21 +0300

    create profile-api

---
+++ src/features/Profile/profile-api.ts
@@ -0,0 +1,18 @@
+import { AxiosResponse } from 'axios'
+
+import { ResponseUpdateUserDataType } from 'common/api/DataTypes'
+import { instance } from 'common/api/main-api'
+
+export const profileAPI = {
+  updateUserData(data: UpdateDataUserType) {
+    return instance.put<UpdateDataUserType, AxiosResponse<ResponseUpdateUserDataType>>(
+      'auth/me',
+      data
+    )
+  },
+}
+
+export type UpdateDataUserType = {
+  name?: string
+  avatar?: string
+}
