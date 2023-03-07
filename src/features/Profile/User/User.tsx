6548ae5e6ef8113b507ed80df68232b346b7150e
yanakorobova <yanaheaven@yandex.ru>
2023-03-07 18:47:05 +0300

    feat: added the ability to change the name

--- src/features/Profile/User/User.tsx
+++ src/features/Profile/User/User.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-import { Typography } from '@mui/material'
 import Avatar from '@mui/material/Avatar'
 import Badge from '@mui/material/Badge'
 
 import s from './User.module.scss'
 
-import { useAppSelector } from 'app/store'
-import edit_name from 'assets/images/edit_name.svg'
+import { useAppDispatch, useAppSelector } from 'app/store'
 import edit_photo from 'assets/images/edit_photo.svg'
-import photo from 'assets/images/photo.svg'
 import { EditableSpan } from 'common/components/EditableSpan/EditableSpan'
+import { changeUserDataTC } from 'features/Profile/profile-reducer'
 
 export const User = () => {
-  const email = useAppSelector<string>(state => state.profile.email) || 'weqwewe@sad.ru'
-  const name = useAppSelector<string>(state => state.profile.name) || 'Alex'
-  const avatar = useAppSelector<string>(state => state.profile.avatar) || photo
-  const editName = () => {}
+  const email = useAppSelector<string>(state => state.profile.email)
+  const name = useAppSelector<string>(state => state.profile.name)
+  const avatar = useAppSelector<string>(state => state.profile.avatar)
+  const dispatch = useAppDispatch()
+  const editName = (newName: string) => {
+    dispatch(changeUserDataTC({ name: newName, avatar }))
+  }
 
   return (
     <>
@@ -30,7 +31,6 @@ export const User = () => {
       <div className={s.information}>
         <div className={s.name}>
           <EditableSpan changeTitle={editName} initTitle={name} />
-          <img src={edit_name} alt="edit" />
         </div>
         <span className={s.email}>{email}</span>
       </div>
