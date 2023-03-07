557c7c69ae2094202800240c5efcc68f2070b73a
yanakorobova <yanaheaven@yandex.ru>
2023-03-07 19:38:38 +0300

    feat: added initialization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import './App.css'
 import { NavLink } from 'react-router-dom'
 
+import { initializeAppTC } from 'app/app-reducer'
+import { useAppDispatch } from 'app/store'
 import { Header } from 'common/components/Header/Header'
 import { RoutesPage } from 'common/components/RoutesPage/RoutesPage'
 import { SnackBar } from 'common/components/SnackBar/SnackBar'
 import { PATH } from 'common/path/path'
 
 export const App = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(initializeAppTC())
+  }, [])
+
   return (
     <div className="App">
       <Header />
