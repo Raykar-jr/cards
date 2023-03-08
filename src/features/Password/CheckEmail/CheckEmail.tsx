758ad84ac871bd27d1d819113d4b8b73aff857a0
Raykar-jr <kirillazarenk@mail.ru>
2023-03-08 17:40:10 +0300

    refactor: remove condition at check email page

--- src/features/Password/CheckEmail/CheckEmail.tsx
+++ src/features/Password/CheckEmail/CheckEmail.tsx
@@ -14,7 +14,6 @@ import { PATH } from 'common/path/path'
 import { checkEmailButton, common_button } from 'common/styles/LoginStyles'
 
 export const CheckEmail = () => {
-  const recoverySuccess = useAppSelector<boolean>(state => state.recovery.success)
   const email = useAppSelector<string>(state => state.recovery.email)
 
   const navigate = useNavigate()
@@ -24,28 +23,26 @@ export const CheckEmail = () => {
 
   return (
     <div>
-      {recoverySuccess && (
-        <Grid container display="flex" justifyContent={'center'}>
-          <Grid item justifyContent={'center'}>
-            <Paper elevation={3} className={s.mainContainer}>
-              <p className={s.h1}>Check email</p>
-              <div>
-                <img src={checkEmailImage} alt="" />
-              </div>
-              <p className={s.optionalText}>We’ve sent an Email with instructions to {email}</p>
-              <Button
-                style={checkEmailButton}
-                sx={common_button}
-                variant={'contained'}
-                color={'primary'}
-                onClick={onClickHandler}
-              >
-                Back to login
-              </Button>
-            </Paper>
-          </Grid>
+      <Grid container display="flex" justifyContent={'center'}>
+        <Grid item justifyContent={'center'}>
+          <Paper elevation={3} className={s.mainContainer}>
+            <p className={s.h1}>Check email</p>
+            <div>
+              <img src={checkEmailImage} alt="" />
+            </div>
+            <p className={s.optionalText}>We’ve sent an Email with instructions to {email}</p>
+            <Button
+              style={checkEmailButton}
+              sx={common_button}
+              variant={'contained'}
+              color={'primary'}
+              onClick={onClickHandler}
+            >
+              Back to login
+            </Button>
+          </Paper>
         </Grid>
-      )}
+      </Grid>
     </div>
   )
 }
