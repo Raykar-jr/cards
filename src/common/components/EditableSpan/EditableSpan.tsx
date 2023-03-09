4efda5159f46a44fb6eefe783e26670031f0c844
Alexander <som17son25@gmail.com>
2023-03-09 21:13:21 +0300

    ref: EditableSpan import from mui

--- src/common/components/EditableSpan/EditableSpan.tsx
+++ src/common/components/EditableSpan/EditableSpan.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, KeyboardEvent, useState } from 'react'
 
-import { TextField } from '@mui/material'
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 
 import s from './EditableSpan.module.scss'
 
