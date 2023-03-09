b44a4b90f37da2fa98031e30cfa997e1f9182b4b
Raykar-jr <kirillazarenk@mail.ru>
2023-03-09 10:25:31 +0300

    feature: add button styles to mui-theme

--- src/common/styles/mui-theme.ts
+++ src/common/styles/mui-theme.ts
@@ -3,6 +3,12 @@ import { createTheme } from '@mui/material/styles'
 export const theme = createTheme({
   typography: {
     fontFamily: ['Montserrat', 'Arial', 'sans-serif'].join(','),
+    button: {
+      textTransform: 'none',
+      fontWeight: 500,
+      fontSize: '1rem',
+      fontStyle: 'normal',
+    },
   },
   palette: {
     primary: {
