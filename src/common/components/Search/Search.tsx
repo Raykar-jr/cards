a5e185913a038f9f89adc5166219959f21eff4ae
yanakorobova <08mickey01@gmail.com>
2023-03-17 02:01:25 +0300

    changed

--- src/common/components/Search/Search.tsx
+++ src/common/components/Search/Search.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 
 import SearchIcon from '@mui/icons-material/Search'
+import { Typography } from '@mui/material'
+import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 
-import s from './Search.module.scss'
+import { useDebounce } from 'common/hooks/use-debounce'
 
-export const Search = () => {
+type SearchPropsType = {
+  onChange: (search: string) => void
+}
+export const Search: React.FC<SearchPropsType> = React.memo(({ onChange }) => {
+  const [value, setValue] = useState<string>('')
+  const debouncedValue = useDebounce<string>(value, 500)
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value)
+  }
+
+  useEffect(() => {
+    onChange(value)
+  }, [debouncedValue])
+
   return (
-    <div className={s.searchContainer}>
-      <p>Search</p>
+    <Grid flex={'0 0 auto'}>
+      <Typography margin={'0 0 8px 0'} fontSize={15} fontWeight={500}>
+        Search
+      </Typography>
       <TextField
+        onChange={onChangeHandler}
+        value={value}
         variant="outlined"
         size="small"
-        InputProps={{ startAdornment: <SearchIcon sx={{ height: '36px' }} /> }}
+        placeholder={'Provide your text'}
+        InputProps={{ startAdornment: <SearchIcon sx={{ height: 36, mr: '5px' }} color={'disabled'} /> }}
       />
-    </div>
+    </Grid>
   )
-}
+})
