5e0a6ddc8ae76d4eade5da571218e01b3d6a3121
yanakorobova <08mickey01@gmail.com>
2023-03-16 17:56:23 +0300

    featL settings panel changed

---
+++ src/common/components/SuperRange/SuperRange.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Slider, SliderProps } from '@mui/material'
+
+export const SuperRange: React.FC<SliderProps> = props => {
+  return (
+    <Slider
+      sx={{
+        width: '155px',
+        color: '#366EFF',
+        '& .MuiSlider-rail': {
+          color: '#366EFF',
+        },
+        '& .MuiSlider-thumb': {
+          color: '#fff',
+          width: '16px',
+          height: '16px',
+          border: '4px solid #366EFF',
+        },
+      }}
+      step={1}
+      min={1}
+      max={100}
+      getAriaLabel={() => 'Temperature range'}
+      {...props}
+    />
+  )
+}
--- src/features/Packs/PackList/Settings/Settings.tsx
+++ src/features/Packs/PackList/Settings/Settings.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import s from './Settings.module.scss'
 
 import { Search } from 'common/components/Search/Search'
+import { RangeCards } from 'features/Packs/PackList/Settings/RangeCards/RangeCards'
 
 export const Settings = () => {
   return (
     <div className={s.settingsBlock}>
       <Search />
+      <RangeCards />
     </div>
   )
 }
