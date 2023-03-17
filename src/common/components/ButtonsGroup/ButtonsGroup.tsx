b08dd4f6af5715abc296298729c74795b4ea29a3
yanakorobova <08mickey01@gmail.com>
2023-03-17 17:34:30 +0300

    feat: change style

--- src/common/components/ButtonsGroup/ButtonsGroup.tsx
+++ src/common/components/ButtonsGroup/ButtonsGroup.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { ButtonGroup, Typography } from '@mui/material'
+import { ButtonGroup, SxProps, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 
@@ -15,31 +15,26 @@ export const ButtonsGroup: React.FC<ButtonsGroupPropsType> = React.memo(({ onCli
   const onClickChanger = (f: () => void) => {
     return () => f()
   }
-  const activeMy = userId ? 'outlined' : 'contained'
-  const activeAll = !userId ? 'outlined' : 'contained'
+
+  const allActive: SxProps = {
+    backgroundColor: !userId ? '#366EFF' : '',
+    width: 80,
+  }
+  const myActive: SxProps = {
+    backgroundColor: userId ? '#366EFF' : '',
+    width: 80,
+  }
 
   return (
     <Grid flex={'0 0 auto'}>
       <Typography margin={'0 0 8px 0'} fontSize={15} fontWeight={500}>
         Show packs cards
       </Typography>
-      <ButtonGroup variant="contained">
-        <Button
-          disabled={!!userId}
-          variant={activeMy}
-          onClick={onClickChanger(onClickMy)}
-          size={'large'}
-          sx={{ width: '80px' }}
-        >
+      <ButtonGroup variant="outlined">
+        <Button disabled={!!userId} onClick={onClickChanger(onClickMy)} size={'large'} sx={myActive}>
           My
         </Button>
-        <Button
-          disabled={!userId}
-          variant={activeAll}
-          onClick={onClickChanger(onClickAll)}
-          size={'large'}
-          sx={{ width: '80px' }}
-        >
+        <Button disabled={!userId} onClick={onClickChanger(onClickAll)} size={'large'} color={'primary'} sx={allActive}>
           All
         </Button>
       </ButtonGroup>
