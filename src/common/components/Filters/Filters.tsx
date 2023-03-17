64c28588757f1a843be13e58c922a4a3442f25ed
yanakorobova <08mickey01@gmail.com>
2023-03-17 16:59:22 +0300

    fix: align-items changed to "flex-start"

--- src/common/components/Filters/Filters.tsx
+++ src/common/components/Filters/Filters.tsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid'
 
 export const Filters: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <Grid container justifyContent={'space-between'} alignItems={'center'}>
+    <Grid container justifyContent={'space-between'} alignItems={'flex-end'}>
       {children}
     </Grid>
   )
