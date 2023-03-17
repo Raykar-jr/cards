c8e11804d961ceb064dd1f07105e73cce017d842
yanakorobova <08mickey01@gmail.com>
2023-03-17 17:00:04 +0300

    feat: added resetButton

--- src/features/Packs/PackList/Settings/FilterPanel.tsx
+++ src/features/Packs/PackList/Settings/FilterPanel.tsx
@@ -4,14 +4,17 @@ import { useAppDispatch, useAppSelector } from 'app/store'
 import { ButtonsGroup } from 'common/components/ButtonsGroup/ButtonsGroup'
 import { Filters } from 'common/components/Filters/Filters'
 import { RangeCards } from 'common/components/RangeCards/RangeCards'
+import { ResetButton } from 'common/components/ResetButton/ResetButton'
 import { Search } from 'common/components/Search/Search'
-import { setQueryParams } from 'features/Packs/packs-reducer'
+import { resetQueryParams, setQueryParams } from 'features/Packs/packs-reducer'
 import { selectUserId } from 'features/Profile/profileSelectors'
 
 export const FilterPanel = () => {
   const dispatch = useAppDispatch()
-  const { min, max } = useAppSelector(state => state.packs.queryParams)
+  const { min, max, packName } = useAppSelector(state => state.packs.queryParams)
   const { page, pageCount } = useAppSelector(state => state.packs.packList)
+
+  console.log(packName)
   const userId = useAppSelector(selectUserId)
   const onSearchChange = useCallback((search: string) => {
     dispatch(setQueryParams({ packName: search }))
@@ -22,12 +25,16 @@ export const FilterPanel = () => {
   const getAllPacks = useCallback(() => {
     dispatch(setQueryParams({ user_id: '' }))
   }, [])
+  const resetFilters = useCallback(() => {
+    dispatch(resetQueryParams())
+  }, [])
 
   return (
     <Filters>
-      <Search onChange={onSearchChange} />
+      <Search onChange={onSearchChange} search={packName} />
       <ButtonsGroup onClickMy={getMyPacks} onClickAll={getAllPacks} />
       <RangeCards />
+      <ResetButton onClick={resetFilters} />
     </Filters>
   )
 }
