f73b63602f8609c4d71b25384d6c8f3437817fee
yanakorobova <08mickey01@gmail.com>
2023-03-17 11:48:44 +0300

    feat: added rangeCards

---
+++ src/common/components/RangeCards/RangeCards.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { SuperRange } from 'common/components/SuperRange/SuperRange'
+
+export const RangeCards = () => {
+  return (
+    <div>
+      <SuperRange />
+    </div>
+  )
+}
