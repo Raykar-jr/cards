93ea405bc91329b86439e9d8264adc71a307b32e
yanakorobova <08mickey01@gmail.com>
2023-03-23 19:09:46 +0300

    feat: changed localstorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,7 +2,6 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from 'redux'
 import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import { createWhitelistFilter } from 'redux-persist-transform-filter'
 import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 
 import { appReducer } from 'app/app-reducer'
@@ -17,7 +16,6 @@ const persistConfig = {
   key: 'packs',
   storage,
   whitelist: ['queryParams'],
-  transforms: [createWhitelistFilter('queryParams', ['user_id'])],
 }
 
 const rootReducer = combineReducers({
