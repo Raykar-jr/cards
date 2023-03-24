868393f175706a8fe60c614549450279d23465dd
yanakorobova <08mickey01@gmail.com>
2023-03-24 17:33:08 +0300

    feat: delete

--- src/features/Learn/learn-api.ts
+++
@@ -1,8 +0,0 @@
-import { UpdateGradeRequestType, UpdateGradeResponseType } from 'common/api/DataTypes'
-import { instance } from 'common/api/main-api'
-
-export const learnApi = {
-  updateGrade(data: UpdateGradeRequestType) {
-    return instance.put<UpdateGradeResponseType>('cards/grade', data)
-  },
-}
